6d74a79e7a0f2be5644e6d1844afeecb3f9c5927
unknown <kapland2003@gmail.com>
2015-11-23 15:51:12 +0200

    modified Binary Search

--- src/edu/kaplan/homework/homework2/MyOwnBinarySearch.java
+++ src/edu/kaplan/homework/homework2/MyOwnBinarySearch.java
@@ -3,14 +3,14 @@ package edu.kaplan.homework.homework2;
 public class MyOwnBinarySearch {
     public static void main(String[] args) {
        int[]array = {1, 4, 7, 10, 11, 13, 15, 17, 18, 19, 22, 30, 48, 49, 66, 69, 72, 89, 94, 97};
-        int element = 69;
+        int element = 97;
         int leftIndex = 0;
-        int rightIndex=array.length - 1;
+        int rightIndex=array.length ;// bug was here
       int result = doBinarySearchRecursion(array, element, leftIndex, rightIndex);
         System.out.println("Index of searching element is " + result);
     }
     public static int doBinarySearchRecursion(int[] array, int key, int left, int right) {
-        int mid = (right + left) / 2;
+        int mid = (left + right) / 2;
         if (left >= right)
             return -1;
         if (array[mid] == key) {
